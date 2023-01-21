d520352383e95354f8b968355e04a7af44451a46
Manish Tiwari <manisht.mt.mt@gmail.com>
2023-01-21 16:21:49 +0530

    added dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
+import Link from "next/link";
 
 import { getSortedPostsData } from "../lib/posts";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import Date from "../components/date";
 
 export default function Home({ allPostsData }) {
   return (
@@ -25,11 +27,11 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
