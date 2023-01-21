a3dc8c39ae70a1fb17710ef3651d521f2480ef0d
Manish Tiwari <manisht.mt.mt@gmail.com>
2023-01-21 12:42:40 +0530

    first nextjs project

---
+++ pages/posts/first-post.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import Script from "next/script";
+import Link from "next/link";
+import Image from "next/image";
+
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <h1>First Post</h1>
+      <h2>
+        &larr; <Link href={"/"}>Back to home</Link>
+      </h2>
+      <Image
+        src={"/images/profile.jpg"}
+        height={144}
+        width={144}
+        alt="Manish Tiwari"
+      />
+    </Layout>
+  );
+}
